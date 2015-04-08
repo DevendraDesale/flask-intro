91f9d161b6a0e466396d0f28c2bb4cbef32e5090
Devendra Desale <devendra.desale@gmail.com>
2015-04-08 12:10:37 +0800

    Adding the user authentications

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, render_template, request, redirect, \
+    url_for,session, flash
+from functools import wraps
 app = Flask(__name__)
+
+
+# This shouldn't be in actual environemnt find better ways.
+# Use random key generator and then use sessions.
+app.secret_key = 'my precious'
+
+
+# Use login required decorator
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 
 # use Decorator for routing
 @app.route('/')
+# Adding login required decorator
+@login_required
 def home():
-    return "Hello, world!"
+    # Remove the string and get the index page now.
+    # return "Hello, world!"
+
+    return render_template("index.html")
 
 
 @app.route('/welcome')
+@login_required
 def welcome():
     return render_template("welcome.html")
 
@@ -21,9 +46,17 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid credentials, Please try again.'
         else:
+            session['logged_in'] = True
+            flash('You have just logged in!')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You have just logged out!')
+    return redirect(url_for('welcome'))
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
