eff48270f351216bdf40d9d44fb1199a55fe2320
Devendra Desale <devendra.desale@gmail.com>
2015-04-08 22:40:06 +0800

    Adding the date database support to the application

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request, redirect, \
-    url_for,session, flash
+    url_for, session, flash, g
 from functools import wraps
+import sqlite3
+
 app = Flask(__name__)
 
 
 # This shouldn't be in actual environemnt find better ways.
 # Use random key generator and then use sessions.
 app.secret_key = 'my precious'
+
+# Configuration fo the sequel lite 3
+app.database = "sample.db"
 
 
 # Use login required decorator
@@ -29,8 +34,12 @@ def login_required(f):
 def home():
     # Remove the string and get the index page now.
     # return "Hello, world!"
-
-    return render_template("index.html")
+    # g is specific to flask which store temporory object.
+    g.db = connect_db()
+    cur = g.db.execute('select * from  posts')
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template("index.html", posts=posts)
 
 
 @app.route('/welcome')
@@ -43,7 +52,8 @@ def welcome():
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        if request.form['username'] != 'admin' or \
+                request.form['password'] != 'admin':
             error = 'Invalid credentials, Please try again.'
         else:
             session['logged_in'] = True
@@ -57,6 +67,14 @@ def logout():
     session.pop('logged_in', None)
     flash('You have just logged out!')
     return redirect(url_for('welcome'))
+
+
+def connect_db():
+    """
+    Getting the sqlite3 DB connection
+    """
+    return sqlite3.connect(app.database)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
