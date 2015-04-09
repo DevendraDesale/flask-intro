c645db077d334d01c42f7ed0db258bfcd94662c7
Devendra Desale <devendra.desale@gmail.com>
2015-04-09 20:15:10 +0800

    Changign the setup to work with the virtualenv wrappers

--- db_create.py
+++ db_create.py
@@ -8,7 +8,7 @@ db.create_all()
 db.session.add(BlogPost("Good", "I'm good."))
 db.session.add(BlogPost("Well", "I'm Well."))
 db.session.add(BlogPost("Fine", "I'm Fine."))
-
+db.session.add(BlogPost("postgres", "we setup a local postgres"))
 
 # Commit the changes
 db.session.commit()
